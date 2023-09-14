8581877e4ab71c6a3076a4e4a5c1c6ee0aa295ae
Eric R. Scott <scottericr@gmail.com>
2023-09-14 14:10:10 -0400

    Initial commit

---
+++ R/01-wrangle.R
@@ -0,0 +1,36 @@
+# This script reads and wrangles data to prepare it for analyses.
+# Eric Scott
+# 2023-09-06
+
+# Load Packages -----
+library(readr)
+library(dplyr)
+
+# Read in data ----
+## Read the csv file ----
+gapminder <- read_csv("data/gapminder_data.csv")
+
+str(gapminder)
+head(gapminder)
+
+
+# Wrangle data ------------------------------------------------------------
+
+gap_1980s <- dplyr::filter(gapminder, year >= 1980, year < 1990)
+
+gap_big <- dplyr::filter(gapminder, pop > 10000000)
+
+
+# Write data --------------------------------------------------------------
+
+write_csv(gap_1980s, "data_clean/gapminder_1980s.csv")
+
+# Write as an R object (useful when saving something more complex than a data frame)
+# write_rds(gap_1980s, "data_clean/gapminder_1980s.rds")
+
+# Save entire workspace (usually a bad idea!)
+# save.image("workspace.Rdata")
+
+
+
+
---
+++ workflow.R
@@ -0,0 +1,6 @@
+
+# first we read and wrangle the data to get just 1980s
+source("R/01-wrangle.R")
+
+# Then we fit a mixed effects model
+source("R/02-analyze.R")
