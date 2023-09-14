bc4cfb12dc1a37971acafdf1f42a237a75b8f6d8
Eric Scott <scottericr@gmail.com>
2023-09-14 14:38:02 -0400

    Plot results

--- R/02-analyze.R
+++ R/02-analyze.R
@@ -9,8 +9,14 @@ gap_1980s <- read_rds("data_clean/gapminder_1980s.rds")
 
 # fit model ---------------------------------------------------------------
 
-#TODO: actually do this modeling!!
+#TODO: actually do this modeling!
 model1 <- lmer()
 
+# adding a plot
+library(ggplot2)
+ggplot(mtcars, aes(mpg, wt)) +
+  geom_point()
+
+ggsave("mtcars.png")
 
 
